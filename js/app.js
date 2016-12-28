95d0a1d856812e9e5622f667dc3ad7b92aa78187
Michael Satow <satow.m@gmail.com>
2016-12-29 10:34:40 +1100

    resize img

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
+	this.imgSrc = ko.observable('img/cat-animal.jpeg');
 	this.imgAttribute = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
 	//this.level = ko.observable('Infant');
 	this.level = ko.computed(function() {

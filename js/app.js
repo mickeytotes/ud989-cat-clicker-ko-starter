773606ffd926494d0708cc113bd7494e97d5bfc0
Michael Satow <satow.m@gmail.com>
2016-12-28 11:21:56 +1100

    add attributes to ViewModel()

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
+	this.imgAttribute = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
 
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	}
 }
 
 ko.applyBindings(new ViewModel());

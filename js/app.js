5a81fa3c2c0bf01beadb41d5fdf6fc4d9247fe54
Michael Satow <satow.m@gmail.com>
2016-12-28 11:54:21 +1100

    fix levels

--- js/app.js
+++ js/app.js
@@ -3,17 +3,26 @@ var ViewModel = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
 	this.imgAttribute = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
-	this.level = ko.observable('Infant');
+	//this.level = ko.observable('Infant');
+	this.level = ko.computed(function() {
+		if(clickCount < 10) {
+			return "Infant";
+		}
+
+		if(clickCount >= 10) {
+			return "Kitten";
+		}
+	}, this);
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	}
 
-	this.changeLevel = ko.computed(function(){
-		if(clickCount > 10 && clickCount < 30) {
-			this.level('Kitten');
-		}
-	}, this);
+	//this.changeLevel = ko.computed(function(){
+	//	if(clickCount > 10) {
+	//		this.level('Kitten');
+	//	}
+	//}, this);
 }
 
 ko.applyBindings(new ViewModel());

69202a503e155f0242b51a0cbb02b1c3cefa308b
Michael Satow <satow.m@gmail.com>
2016-12-28 11:47:58 +1100

    add levels

--- js/app.js
+++ js/app.js
@@ -3,10 +3,17 @@ var ViewModel = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
 	this.imgAttribute = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
+	this.level = ko.observable('Infant');
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	}
+
+	this.changeLevel = ko.computed(function(){
+		if(clickCount > 10 && clickCount < 30) {
+			this.level('Kitten');
+		}
+	}, this);
 }
 
 ko.applyBindings(new ViewModel());

404cd7ee858489ee54e52ee756380aaa02a6d0a2
Michael Satow <satow.m@gmail.com>
2016-12-29 11:17:28 +1100

    separate out model

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
-var ViewModel = function() {
+var Cat = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/cat-animal.jpeg');
 	this.imgAttribute = ko.observable('https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg');
-	//this.level = ko.observable('Infant');
+	this.nickNames = ko.observableArray([
+		{nName: 'Dirk'},
+		{nName: 'Tchaikovski'},
+		{nName: 'Coolio'},
+		{nName: 'Bingo'},
+		{nName: 'Greg Allman'}
+		]);
+
 	this.level = ko.computed(function() {
 		if(this.clickCount() < 10) {
 			return "Infant";
@@ -18,18 +25,15 @@ var ViewModel = function() {
 			return "Young Scrappy";
 		}
 	}, this);
+}
+
+var ViewModel = function() {
+
+	this.currentCat = ko.observable(new Cat());
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	}
-
-	this.nickNames = ko.observableArray([
-		{nName: 'Dirk'},
-		{nName: 'Tchaikovski'},
-		{nName: 'Coolio'},
-		{nName: 'Bingo'},
-		{nName: 'Greg Allman'}
-		]);
 }
 
 ko.applyBindings(new ViewModel());
